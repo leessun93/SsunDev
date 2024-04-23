53f980de3d8ca38c1c8bb74da7e020a4db0698a2
leessun93 <dltjsgma@naver.com>
2024-04-23 16:54:27 +0900

    Initial commit

---
+++ src/main/java/com/leeSsunCompany/SsunDev/SsunDevApplication.java
@@ -0,0 +1,13 @@
+package com.leeSsunCompany.SsunDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsunDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsunDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeSsunCompany/SsunDev/SsunDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeSsunCompany.SsunDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsunDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
